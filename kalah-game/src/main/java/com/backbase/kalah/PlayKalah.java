630b4eb6e2e9ccd44e17b019301195c1d93f75b1
Sachith Dickwella <sachith_prasanna@live.com>
2020-01-12 00:50:11 +0800

    Complete dev of core game logic

--- kalah-game/src/main/java/com/backbase/kalah/PlayKalah.java
+++ kalah-game/src/main/java/com/backbase/kalah/PlayKalah.java
@@ -2,13 +2,15 @@ package com.backbase.kalah;
 
 import com.backbase.kalah.records.GameStatus;
 import com.backbase.kalah.util.InvalidPitUserException;
+import com.backbase.kalah.util.ServiceConstance.Player;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.Map;
 
-import static com.backbase.kalah.util.ServiceConstance.STORE_INDEX;
 import static com.backbase.kalah.util.ServiceConstance.PIT_COUNT;
+import static com.backbase.kalah.util.ServiceConstance.STORE_INDEX;
 
 /**
  * Stereo type {@link Component} class to bind with {@link org.springframework.context.ApplicationContext}
@@ -23,7 +25,7 @@ public class PlayKalah {
      * Core game algorithm goes here. Given {@link GameStatus} instance coming from upstream function could
      * be a fresh game instance or old game instance started while back. Either way incoming object is coming
      * from the Redis store and update status accordingly.
-     * <p>
+     *
      * Nothing return from this function as {@link GameStatus} instance pass by reference. Therefore, all the
      * changes do to the respective instance would reflect on the upstream object.
      *
@@ -42,9 +44,11 @@ public class PlayKalah {
         }
 
         final var board = Collections.synchronizedMap(gameStatus.getBoard());
-        final int pickedUpSeeds = Integer.parseInt(board.get(pickupPitId));
-
-        board.put(pickupPitId, "0");
+        final int pickedUpSeeds = grabSeeds(board, pickupPitId);
+        if (pickedUpSeeds == 0) {
+            throw new InvalidPitUserException(gameStatus, "Chosen pit %d, is empty, %s's got another chance",
+                    pickupPitId, player);
+        }
 
         int nextPitId = pickupPitId + 1;
         for (int i = 1; i <= pickedUpSeeds; i++) {
@@ -52,10 +56,79 @@ public class PlayKalah {
                 nextPitId = player.firstPit();
             }
 
-            board.put(nextPitId, String.valueOf(Integer.parseInt(board.get(nextPitId)) + 1));
+            if (i == pickedUpSeeds) {
+                var shouldToggle = lastPitUpdate(board, player, nextPitId);
+                gameStatus.player(shouldToggle);
+            } else {
+                justPut1toPit(board, nextPitId);
+            }
+
             nextPitId++;
         }
+    }
 
-        gameStatus.player(true);
+    /**
+     * Grab seeds from the pit indicate by the parameter {@code pickupPit} value. By grabbing the seeds in a pit,
+     * this function just return the number of seeds as well as empty (zero out) the pit.
+     *
+     * @param board     instance of {@link Map} that represent the board.
+     * @param pickupPit {@code int} value which pit's seeds should be grabbed and return.
+     * @return number seeds {@code int} value in the pit.
+     */
+    private int grabSeeds(@NotNull Map<Integer, String> board, final int pickupPit) {
+        int seeds = Integer.parseInt(board.get(pickupPit));
+        if (seeds > 0) board.put(pickupPit, "0");
+
+        return seeds;
+    }
+
+    /**
+     * Just insert new seed into the pit indicate by given {@code pitId} parameter. Just update the pit value
+     * by addition one into the pit's existing value.
+     *
+     * @param board instance of {@link Map} instance that represent the board.
+     * @param pitId {@code pitId} value indicate which pit to be updated.
+     */
+    private void justPut1toPit(@NotNull Map<Integer, String> board, final int pitId) {
+        board.put(pitId, String.valueOf(Integer.parseInt(board.get(pitId)) + 1));
+    }
+
+    /**
+     * Serve during the last pit update in a round of sowing seeds. Based on which pit becoming the last pit
+     * of a round, this function updates the board {@link Map} instance's respective pits.
+     *
+     * If the last pit would become the store pit/kalah, then the player gets another chance to sow seeds
+     * across him/her pits, but do not get any seeds from the opponent.
+     *
+     * Otherwise, if the last pit would become a usual pit/house of the player's own and that pit is empty
+     * {@code (value = 0)}, then the player get to collect all the seeds from the opponent's exact opposite
+     * pit and put them and player's own single seed into the player's store/kalah and then the opponent get
+     * the chance to play.
+     *
+     * If the last pit is not the store/kalah of the player, or the general pit/house is not empty, then, just
+     * put single seed into the pit and toggle the player to give the chance to next player.
+     *
+     * @param board   instance of {@link Map} that represent the board.
+     * @param player  instance of {@link Player} that represent the currently active player.
+     * @param lastPit {@code int} value of the last pit of the round.
+     * @return {@code boolean} value indicating whether player need to toggle or not.
+     */
+    private boolean lastPitUpdate(@NotNull Map<Integer, String> board, @NotNull Player player, final int lastPit) {
+        int storeIndex = player.number() * STORE_INDEX;
+        if (lastPit != storeIndex) {
+            if (Integer.parseInt(board.get(lastPit)) == 0) {
+                int opponentPit = lastPit < STORE_INDEX ? lastPit + STORE_INDEX : lastPit - STORE_INDEX;
+                int opponentSeeds = grabSeeds(board, opponentPit);
+                int newStoreSeedCount = Integer.parseInt(board.get(storeIndex)) + opponentSeeds + 1;
+
+                board.put(storeIndex, String.valueOf(newStoreSeedCount));
+            } else {
+                justPut1toPit(board, lastPit);
+            }
+            return true;
+        } else {
+            justPut1toPit(board, lastPit);
+            return false;
+        }
     }
 }
